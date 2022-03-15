d55f57180404f38c344ac5fb24552432013ce02e
Kuptsov <KuptsovEV@lad24.ru>
2022-03-15 22:38:34 +0300

    hometask 10.2

---
+++ Part 1/10.2 User Errors/extendsOfSyntaxError.js
@@ -0,0 +1,19 @@
+
+class FormatError extends SyntaxError {
+
+    constructor(message) {
+        super(message);
+        this.name = 'FormatError';
+    }
+
+}
+
+
+let err = new FormatError("ошибка форматирования");
+
+console.log( err.message ); // ошибка форматирования
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof FormatError ); // true
+console.log( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
